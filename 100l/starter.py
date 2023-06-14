66aadbbcb4fdc651b04dd1fdc5384a49b4651b71
k07ter <kostin-g@yandex.ru>
2023-06-14 14:21:27 +0300

    changes for building a dockerfile

--- 100l/starter.py
+++ 100l/starter.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python3
 
 import sys, os
-import asyncio
-from flask import *  #Flask, render_template
+from flask import *
 from datetime import datetime
 from img_chk import to_chk
 
-#load_dotenv('.env1')
-#with open('.env1', 'r') as env:
-#    _env = json.load(env)
-#    print(_env)
-#print(os.environ.get('tag1'))
-
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tskdlr.db"
-#db = SQLAlchemy(app)
-
-#class TehZadacha(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    task_id = db.Column(db.String, nullable=False)
-#    descr = db.Column(db.String, nullable=False)
-    #tsk_type = db.Column(db.String(10), from_choices={'FIELD': [(1, 'high'), (2, 'middle'), (3,'low')]})
-#    assigned = db.Column(db.Integer)
-#    datetime = db.Column(db.DateTime, default=datetime.utcnow)
-#    isActive = db.Column(db.Boolean, default=True)
-
-#    def __repe__(self):
-#        return f'%s' % self.descr
 
 @app.route('/', methods=['GET'])
 def index():
@@ -46,19 +25,5 @@ def success():
 def file_loader():
     return render_template('file_loader.html')
 
-#async def form_maket():
-#    await asyncio.sleep(5)
-    #app = QtWidgets.QApplication([])
-    #mon = Monitor()
-    #print('mon')
-    #await asyncio.sleep(1)
-    #return 'done'
-
-#async def check_side():
-#    print('Testirovanie')
-#    await asyncio.sleep(1)
-#    return 'done'
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5001, host='0.0.0.0')
